72ec8ef48b4a3bba2ca83e942c1751074fa46c74
Cho-Ching Lu <chochinlu@gmail.com>
2019-07-22 10:46:08 +0800

    seperate the hit function

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+function hitOri(fallenApple, target) {
+  const { x, y, z, r } = fallenApple;
+
+  const safeZ = target.z - target.r > z + r;
+  const safeX = target.x + target.r < x - r || target.x - target.r > x + r;
+  const safeY = target.y + target.r < y - r || target.y - target.r > y + r;
+
+  return !safeZ && (!safeX || !safeY);
+}
+
+const hit = (fallenApple, target) => hitOri(fallenApple, target);
+
 function appleFall(apples, i) {
   if (apples.length === 0 || apples.length === 1) {
     return '0';
   }
 
-  // 將一開始掉下的蘋果與開頭蘋果對調, 左邊的蘋果就是會掉下的, 右邊陣列的蘋果就是沒有掉下的
   if (i !== 0) {
     const temp = apples[0];
     apples[0] = apples[i];
@@ -17,16 +28,11 @@ function appleFall(apples, i) {
     let compareIndex = fallenTotal;
 
     const fallenApple = apples[checkedFallenIndex];
-    const { x, y, z, r } = fallenApple;
 
     while (compareIndex < apples.length) {
       const target = apples[compareIndex];
 
-      const safeZ = target.z - target.r > z + r;
-      const safeX = target.x + target.r < x - r || target.x - target.r > x + r;
-      const safeY = target.y + target.r < y - r || target.y - target.r > y + r;
-
-      if (!safeZ && (!safeX || !safeY)) {
+      if (hit(fallenApple, target)) {
         const temp = apples[fallenTotal];
         apples[fallenTotal] = apples[compareIndex];
         apples[compareIndex] = temp;
